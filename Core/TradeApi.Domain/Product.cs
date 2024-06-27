21f39d868922c4ff3f7f5058a5d11f0d0f30783d
Mehmet Altınkaynak <m.altinkaynak14@gmail.com>
2024-06-27 17:07:39 +0300

    Temel Entityler Oluşturuldu

---
+++ Core/TradeApi.Domain/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeApi.Domain.Common;
+
+namespace TradeApi.Domain
+{
+    public class Product : EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set;}
+
+        public Brand Brand { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        //public required string ImagePath { get; set;}
+    }
+}
