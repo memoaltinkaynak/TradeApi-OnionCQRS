2f7cf75e432f0408f32b1ddea9e2062a97614c1a
Mehmet Altınkaynak <m.altinkaynak14@gmail.com>
2024-07-10 17:36:53 +0300

    MediatR Eklendi application katmanınad Features içerisinde Product ile alakalı işlemler yapıldı application katmanı için dependency injection işlemi yapıldı

---
+++ Core/TradeApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryRequest : IRequest<IList<GetAllProductsQueryResponse>>
+    {
+    }
+}
