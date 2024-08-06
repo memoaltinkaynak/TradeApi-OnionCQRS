8ee8315b1abda005241b88b954bc9a2e8f9aacb1
Mehmet Altınkaynak <m.altinkaynak14@gmail.com>
2024-08-06 12:32:11 +0300

    Project rules tamamlandı . Base rules ypısı eklendi

--- Core/TradeApi.Application/Features/Products/Command/CreateProduct/CreateProducutCommandHandler.cs
+++ Core/TradeApi.Application/Features/Products/Command/CreateProduct/CreateProducutCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using TradeApi.Application.Features.Products.Rules;
 using TradeApi.Application.UnitOfWorks;
 using TradeApi.Domain.Entities;
 
@@ -7,14 +8,21 @@ namespace TradeApi.Application.Features.Products.Command.CreateProduct
     public class CreateProducutCommandHandler : IRequestHandler<CreateProductCommandRequest, Unit>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly ProductRules productRules;
 
-        public CreateProducutCommandHandler(IUnitOfWork unitOfWork)
+        public CreateProducutCommandHandler(IUnitOfWork unitOfWork, ProductRules productRules)
         {
             this.unitOfWork = unitOfWork;
+            this.productRules = productRules;
         }
         public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
+            IList<Product> products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            await productRules.ProductTitleMustNotBeSame(products, request.Title);
+
             Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
+
 
             await unitOfWork.GetWriteRepository<Product>().AddAsync(product);
             if (await unitOfWork.SaveAsync() > 0)
