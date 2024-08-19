af78f6e7f709907f79d73f7eaf39f1426a7b5d8c
Mehmet Altınkaynak <m.altinkaynak14@gmail.com>
2024-08-19 11:02:22 +0300

    Mediatr ve Jwt ile Login işlemleri yapıldı

---
+++ Core/TradeApi.Application/Features/Auth/Exceptions/EmailOrPasswordShouldNotBeInvalid.cs
@@ -0,0 +1,9 @@
+﻿using TradeApi.Application.Bases;
+
+namespace TradeApi.Application.Features.Auth.Exceptions
+{
+    public class EmailOrPasswordShouldNotBeInvalid : BaseException
+    {
+        public EmailOrPasswordShouldNotBeInvalid() : base("Kullanıcı veya şifre hatalı!") { }
+    }
+}
--- Presentation/TradeApi.Api/Controllers/AuthController.cs
+++ Presentation/TradeApi.Api/Controllers/AuthController.cs
@@ -1,12 +1,13 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TradeApi.Application.Features.Auth.Command.Login;
 using TradeApi.Application.Features.Auth.Command.Register;
 
 namespace TradeApi.Api.Controllers
 {
     [Route("api/[controller]/[action]")]
-    [ApiController]
+    [ApiController] 
     public class AuthController : ControllerBase
     {
         private readonly IMediator mediator;
@@ -21,6 +22,13 @@ namespace TradeApi.Api.Controllers
         {
             await mediator.Send(request);
             return StatusCode(StatusCodes.Status201Created);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginCommandRequest request)
+        {
+            var response = await mediator.Send(request);
+            return StatusCode(StatusCodes.Status200OK, response);
         }
     }
 }
